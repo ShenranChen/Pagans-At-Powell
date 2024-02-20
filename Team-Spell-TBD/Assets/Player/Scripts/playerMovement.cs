1dcdf4eedad1777a00c620adc304429a3f610f23
daniekomarley <61295346+daniekomarley@users.noreply.github.com>
2024-02-20 00:10:16 -0800

    Player Movement, Camera Movement, Roll
    added player movement
    added camera movement
    added a roll ability

--- Team-Spell-TBD/Assets/Player/Scripts/playerMovement.cs
+++ Team-Spell-TBD/Assets/Player/Scripts/playerMovement.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// Make this script a component of the: Player Game Object
+
 public class playerMovement : MonoBehaviour
 {
+    // Public variables
+
     // Private variables
     private Rigidbody2D rb;
     private Vector2 inputVector;
+    private float disableMovementTimer = 0f;
 
     // Serialized private variables
     [SerializeField] private float playerSpeed = 5f;
@@ -22,12 +27,18 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = inputVector * playerSpeed;
+        if (disableMovementTimer <= 0) { rb.velocity = inputVector * playerSpeed; }
+        else { disableMovementTimer -= Time.deltaTime; }
     }
 
     // OnMove is called on button press of move action
     void OnMove(InputValue val)
     {
         inputVector = val.Get<Vector2>().normalized;
+    }
+
+    public void disableMovementFor(float timeInSeconds)
+    {
+        disableMovementTimer = timeInSeconds;
     }
 }
